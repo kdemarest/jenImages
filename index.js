2a790eee3b62a4f0fcf20fd989af043e23795bd9
Ken Demarest <ken.demarest@gmail.com>
2020-08-24 16:53:54 -0500

    cleanup

--- index.js
+++ index.js
@@ -1,32 +1,66 @@
+
 function execShellCommand(cmd) {
 	const exec = require('child_process').exec;
 	return new Promise((resolve, reject) => {
 		exec(cmd, (error, stdout, stderr) => {
+			let result = {};
 			if (error) {
-				console.warn(error);
+				result.error = error;
 			}
-			resolve(stdout? stdout : stderr);
+			result.message = stdout? stdout : stderr;
+			resolve(result);
 		});
 	});
 }
 
-let commandList = `
-git pull
-git add .
-git commit -m "no comment"
-git push --force
-`.trim().split('\n');
-
 async function main() {
 
-	console.log('Publishing images now.');
-	for( let i=0 ; i<commandList.length ; ++i ) {
-		console.log(commandList[i]);
-		let result = await execShellCommand( commandList[i] );
-		console.log(result);
+	function log(data) {
+		const fs = require('fs');
+		fs.appendFile('history.log', data+'\n', function (err) {
+			if (err) throw err;
+		});
 	}
+
+	async function run(cmd,showErrors=true) {
+		console.log(cmd);
+		let result = await execShellCommand( cmd );
+		log( cmd );
+		log( JSON.stringify(result) );
+		if( result.error ) {
+			console.log(result);
+		}
+		return result;
+	}
+
+	async function addImage(fileSpec) {
+		let status = await run('git status '+fileSpec+' -s');
+		if( status.message != '' ) {
+			console.log(status.message.trim());
+			let result = await run('git add '+fileSpec);
+			if( result.message !== '' ) {
+				console.log(result);
+			}
+			imageCount += result.message.split('\n').length;
+		}
+	}
+
+	log( '\nRUN: '+new Date().toISOString() );
+	let imageCount = 0;
+	await run('git pull');
+	await addImage('*.png');
+	await addImage('*.jpg');
+	await addImage('*.jpeg');
+	if( imageCount <= 0 ) {
+		console.log("Nothing to do.");
+	}
+	else {
+		await run('git commit -m "updating '+imageCount+' images"');
+		await run('git push --force');
+	}
+
 	console.log( 'Published.' );
-	console.log( 'You can view an image on the browser at https://kdemarest.github.io/jenemail/IMAGE_NAME.png' );
+	console.log( 'You can view an image on the browser at:\nhttps://kdemarest.github.io/jenemail/IMAGE_NAME.png' );
 }
 
 main();
